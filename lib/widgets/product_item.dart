43dfbeeae0f2b8a472527a975fbd948628d084cc
edoardopaolinelli <edoardo.paolinelli@gmail.com>
2022-09-27 17:36:58 +0200

    Aggiunta la schermata di visualizzazione dei prodotti, aggiunti dei prodotti fittizi di prova, modificati i fonts utilizzati

---
+++ lib/widgets/product_item.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class ProductItem extends StatelessWidget {
+  final String id;
+  final String title;
+  final String imageUrl;
+
+  const ProductItem({
+    super.key,
+    required this.id,
+    required this.title,
+    required this.imageUrl,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GridTile(
+      footer: GridTileBar(
+        leading: IconButton(
+          onPressed: () {},
+          icon: const Icon(Icons.shopping_bag),
+          color: Colors.deepOrange,
+        ),
+        trailing: IconButton(
+          onPressed: () {},
+          icon: const Icon(Icons.favorite),
+          color: Colors.deepOrange,
+        ),
+        backgroundColor: Colors.black45,
+        title: Text(
+          title,
+          textAlign: TextAlign.center,
+        ),
+      ),
+      child: Container(
+        decoration: BoxDecoration(
+          border: Border.all(
+            width: 3.5,
+            color: Colors.grey.shade800,
+          ),
+        ),
+        child: Image.network(
+          imageUrl,
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
+}
