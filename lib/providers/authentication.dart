1b0528bae93d80ce7ba01eb4a9fe61de62aa7343
edoardopaolinelli <edoardo.paolinelli@gmail.com>
2022-10-12 10:51:43 +0200

    Refactoring, per evitare codice duplicato

--- lib/providers/authentication.dart
+++ lib/providers/authentication.dart
@@ -32,82 +32,52 @@ class Authentication with ChangeNotifier {
     return null;
   }
 
-  Future<void> signup(String email, String password) async {
+  Future<void> _authenticate(
+      String email, String password, String urlSegment) async {
     final url = Uri.parse(
-        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=$apiKey');
-    try {
-      final response = await http.post(
-        url,
-        body: json.encode(
-          {
-            'email': email,
-            'password': password,
-            'returnSecureToken': true,
-          },
-        ),
-      );
-      final responseData = json.decode(response.body);
-      if (responseData['error'] != null) {
-        throw HttpException(responseData['error']['message']);
-      }
-      _token = responseData['idToken'];
-      _userId = responseData['localId'];
-      _expirationDate = DateTime.now().add(
-        Duration(
-          seconds: int.parse(responseData['expiresIn']),
-        ),
-      );
-      _autoLogout();
-      notifyListeners();
-      final preferences = await SharedPreferences.getInstance();
-      final userData = json.encode({
-        'token': _token,
-        'userId': _userId,
-        'expirationDate': _expirationDate!.toIso8601String(),
-      });
-      preferences.setString('userData', userData);
-    } catch (error) {
-      throw error;
+        'https://identitytoolkit.googleapis.com/v1/$urlSegment?key=$apiKey');
+    //try {
+    final response = await http.post(
+      url,
+      body: json.encode(
+        {
+          'email': email,
+          'password': password,
+          'returnSecureToken': true,
+        },
+      ),
+    );
+    final responseData = json.decode(response.body);
+    if (responseData['error'] != null) {
+      throw HttpException(responseData['error']['message']);
     }
+    _token = responseData['idToken'];
+    _userId = responseData['localId'];
+    _expirationDate = DateTime.now().add(
+      Duration(
+        seconds: int.parse(responseData['expiresIn']),
+      ),
+    );
+    _autoLogout();
+    notifyListeners();
+    final preferences = await SharedPreferences.getInstance();
+    final userData = json.encode({
+      'token': _token,
+      'userId': _userId,
+      'expirationDate': _expirationDate!.toIso8601String(),
+    });
+    preferences.setString('userData', userData);
+    /* } catch (error) {
+      throw error;
+    } */
+  }
+
+  Future<void> signup(String email, String password) async {
+    return _authenticate(email, password, 'accounts:signUp');
   }
 
   Future<void> login(String email, String password) async {
-    final url = Uri.parse(
-        'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=$apiKey');
-    try {
-      final response = await http.post(
-        url,
-        body: json.encode(
-          {
-            'email': email,
-            'password': password,
-            'returnSecureToken': true,
-          },
-        ),
-      );
-      final responseData = json.decode(response.body);
-      if (responseData['error'] != null) {
-        throw HttpException(responseData['error']['message']);
-      }
-      _token = responseData['idToken'];
-      _userId = responseData['localId'];
-      _expirationDate = DateTime.now().add(
-        Duration(
-          seconds: int.parse(responseData['expiresIn']),
-        ),
-      );
-      _autoLogout();
-      notifyListeners();
-      final preferences = await SharedPreferences.getInstance();
-      final userData = json.encode({
-        'token': _token,
-        'userId': _userId,
-        'expirationDate': _expirationDate!.toIso8601String(),
-      });
-      preferences.setString('userData', userData);
-    } catch (error) {
-      throw error;
-    }
+    return _authenticate(email, password, 'accounts:signInWithPassword');
   }
 
   Future<bool> automaticLogin() async {
